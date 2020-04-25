118db399bd88fc7d8266366f14a202b951060827
Young Kim <yk465@cornell.edu>
2020-04-25 00:24:56 -0400

    Init some more methods

--- backend/main.py
+++ backend/main.py
@@ -3,9 +3,30 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+############################################### HELLO WORLD
+
+@app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
+
+############################################### OTHER ROUTES
+
+# Get user's profile
+@app.route('/profile', methods=['GET'])
+def profile():
+    return 'yo'
+
+# Get list of all users?
+@app.route('/users', methods=['GET'])
+def get_users():
+    return 'yo'
+
+# Send slack message to a specific user?
+@app.route('/slack/message', methods=['POST'])
+def send_message():
+    return 'yo'
+
+############################################### ENTRYPOINT
 
 if __name__ == "__main__":
     app.run(debug=True)
