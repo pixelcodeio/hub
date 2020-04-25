de31ff6fe656d1d0dd2fc3532bd5df7edbfec2bd
Young Kim <yk465@cornell.edu>
2020-04-25 00:53:51 -0400

    Init oauth code

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+import requests
+
 app = Flask(__name__)
 app.debug = True
 
@@ -8,6 +10,21 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
+
+############################################### OAUTH
+
+@app.route('/oauth-test', methods=['GET'])
+def oauth_test():
+    return '<a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=1085915442018.1079182647094"><img alt=""Sign in with Slack"" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>'
+
+@app.route('/oauth', methods=['POST'])
+def oauth():
+    CLIENT_ID = "1085915442018.1079182647094"
+    CLIENT_SECRET = "e52cf377e47cf40ee0e70f5f30505cd8"
+
+    BASE_URL = "https://slack.com/api/oauth.v2.access?client_id={}&client_secret={}&code={}"
+
+    requests.get(BASE_URL.format(CLIENT_ID, CLIENT_SECRET, CODE))
 
 ############################################### OTHER ROUTES
 
