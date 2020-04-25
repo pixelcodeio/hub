23aa94f4812e0fd98e53a7c36fe1f23d0590581b
Young Kim <yk465@cornell.edu>
2020-04-25 03:14:47 -0400

    Refactored some code my brain hurts

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.debug = True
 slack_events_adapter = SlackEventAdapter(
     'abfc6945359193db5006ee441bffefdd', "/slack/events", app)
 
-user_list = []
+user_list = get_users()
+profiles_dict = {}
 
 ############################################### HELLO WORLD
 
@@ -23,42 +24,53 @@ def hello_world():
 
 ############################################### OAUTH
 
+def generate_oauth_url(redirect_uri):
+    return '<a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=1085915442018.1079182647094&redirect_uri={}"><img alt=""Sign in with Slack"" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>'.format(redirect_uri)
+
 @app.route('/signup', methods=['GET'])
 def signup():
     redirect_uri = os.environ["SERVER_URL"] + "/oauthv2"
-    return '<a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=1085915442018.1079182647094&redirect_uri={}"><img alt=""Sign in with Slack"" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>'.format(redirect_uri)
+    return generate_oauth_url(redirect_uri)
 
 @app.route('/signin', methods=['GET'])
 def signin():
     redirect_uri = os.environ["SERVER_URL"] + "/oauth"
-    return '<a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=1085915442018.1079182647094&redirect_uri={}"><img alt=""Sign in with Slack"" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>'.format(redirect_uri)
+    return generate_oauth_url(redirect_uri)
+
+def oauth_common(code, redirect_uri, base_url):
+    CLIENT_ID = "1085915442018.1079182647094"
+    CLIENT_SECRET = "e52cf377e47cf40ee0e70f5f30505cd8"
+
+    response = requests.get(base_url.format(CLIENT_ID, CLIENT_SECRET, code, redirect_uri))
+    response_json = json.loads(response.text)
+
+    profiles_dict[response_json["user_id"]] = response_json
+
+    return {
+        "user_id": response_json["user_id"],
+        "user_image": response_json["user"]["image_1024"],
+        "team_image": response_json["team"]["image_230"],
+        "team_name": response_json["team"]["name"],
+    }
+
 
 @app.route('/oauth', methods=['GET'])
 def oauth():
     code = request.args.get('code')
     redirect_uri = os.environ["SERVER_URL"] + "/oauth"
+    base_url = "https://slack.com/api/oauth.access?client_id={}&client_secret={}&code={}&redirect_uri={}"
 
-    CLIENT_ID = "1085915442018.1079182647094"
-    CLIENT_SECRET = "e52cf377e47cf40ee0e70f5f30505cd8"
+    return oauth_common(code, redirect_uri, base_url)
 
-
-    BASE_URL = "https://slack.com/api/oauth.access?client_id={}&client_secret={}&code={}&redirect_uri={}"
-    response = requests.get(BASE_URL.format(CLIENT_ID, CLIENT_SECRET, code, redirect_uri))
-
-    return response.text
 
 @app.route('/oauthv2', methods=['GET'])
 def oauthv2():
     code = request.args.get('code')
     redirect_uri = os.environ["SERVER_URL"] + "/oauthv2"
 
-    CLIENT_ID = "1085915442018.1079182647094"
-    CLIENT_SECRET = "e52cf377e47cf40ee0e70f5f30505cd8"
+    base_url = "https://slack.com/api/oauth.v2.access?client_id={}&client_secret={}&code={}&redirect_uri={}"
 
-    BASE_URL = "https://slack.com/api/oauth.v2.access?client_id={}&client_secret={}&code={}&redirect_uri={}"
-    response = requests.get(BASE_URL.format(CLIENT_ID, CLIENT_SECRET, code, redirect_uri))
-
-    return response.text
+    return oauth_common(code, redirect_uri, base_url)
 
 ############################################### OTHER ROUTES
 
@@ -92,5 +104,4 @@ def update_emoji(payload):
 
 
 if __name__ == "__main__":
-    user_list = get_users()
     app.run(debug=True)
