4b2c14c467674e2a0af6e292c675dc67825fe01d
Young Kim <yk465@cornell.edu>
2020-04-26 01:14:55 -0400

    bruh

--- src/views/Home/Components/Celebrations.tsx
+++ src/views/Home/Components/Celebrations.tsx
@@ -14,6 +14,61 @@ export interface CelebrationsComponentProps extends DispatchProps {
   homepage?: Homepage
 }
 
+function pop (e) {
+  e.stopPropagation();
+  console.log(e.clientX, e.clientY, e.pageX, e.pageY);
+  for (let i=0; i < 30; i++) {
+    createParticle(e.pageX, 0);
+  }
+  console.log("HERE")
+}
+
+function createParticle (x, y) {
+  const particle = document.createElement('particle');
+  document.getElementById("root")?.appendChild(particle);
+  
+  // Calculate a random size from 5px to 25px
+  const size = Math.floor(Math.random() * 20 + 5);
+  particle.style.left = x
+  particle.style.top = y
+  particle.style.width = `${size}px`;
+  particle.style.height = `${size}px`;
+  particle.style.position = 'absolute'
+  particle.style.zIndex = "10000"
+  // Generate a random color in a blue/purple palette
+  particle.style.background = `hsl(${Math.random() * 90 + 180}, 70%, 60%)`;
+  
+  // Generate a random x & y destination within a distance of 75px from the mouse
+  const destinationX = x + (Math.random() - 0.5) * 2 * 75;
+  const destinationY = y + (Math.random() - 0.5) * 2 * 75;
+
+  // Store the animation in a variable as we will need it later
+  const animation = particle.animate([
+    {
+      // Set the origin position of the particle
+      // We offset the particle with half its size to center it around the mouse
+      transform: `translate(-50%, -50%) translate(${x}px, ${y}px)`,
+      opacity: 1
+    },
+    {
+      // We define the final coordinates as the second keyframe
+      transform: `translate(${destinationX}px, ${destinationY}px)`,
+      opacity: 0
+    }
+  ], {
+    // Set a random duration from 500 to 1500ms
+    duration: Math.random() * 1000 + 50000,
+    easing: 'cubic-bezier(0, .9, .57, 1)',
+    // Delay every particle with a random value of 200ms
+    delay: Math.random() * 200
+  });
+  
+  // When the animation is complete, remove the element from the DOM
+  animation.onfinish = () => {
+    particle.remove();
+  };
+}
+
 export const CelebrationsComponent: React.FC<CelebrationsComponentProps> = ({ homepage }) => {
   const history = useHistory()
   const birthdays = homepage?.birthdays
@@ -50,7 +105,9 @@ export const CelebrationsComponent: React.FC<CelebrationsComponentProps> = ({ ho
                     <Text variant="body2">{`${birthday.title} • ${birthday.team}`}</Text>
                   </Box>
                 </Box>
-                <Button border={`1px solid ${colors.gray2}`} borderRadius={4} padding={10} style={{ fontSize: "16px" }}>{"🎂"}</Button>
+                <div onClick={pop}>
+                  <Button border={`1px solid ${colors.gray2}`} borderRadius={4} padding={10} style={{ fontSize: "16px" }}>{"🎂"}</Button>
+                </div>
               </Box>
             </CelebrationBox>
             <Spacer mt={1} />
