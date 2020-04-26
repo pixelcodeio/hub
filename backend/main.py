f717dca798f40324e5cd8523b2a804d6d085b2ba
Omar Rasheed <oyr2@cornell.edu>
2020-04-26 03:58:01 -0700

    Update linter lol

--- backend/main.py
+++ backend/main.py
@@ -4,16 +4,15 @@ import os
 import random
 from functools import reduce
 from urllib.parse import urlparse
-import requests
 
 from flask import Flask, request
-from flask_cors import CORS
 
 import requests
+import slackbot
+from daily_questions import daily_questions
+from flask_cors import CORS
 from init_profiles import init_profiles
 from models import Anniversary, Birthday, DailyQuestion, NewHire, Poll, Thank
-from slackbot import *
-from daily_questions import daily_questions
 from slackeventsapi import SlackEventAdapter
 
 app = Flask(__name__)
@@ -21,7 +20,7 @@ app.debug = True
 cors = CORS(app)
 
 slack_events_adapter = SlackEventAdapter(
-    'abfc6945359193db5006ee441bffefdd', "/slack/events", app)
+    'f819aca71451dcbe8e80c99ad3adbb17', "/slack/events", app)
 
 # daily_questions = [
 #     'What is your favorite TV Show?',
@@ -40,34 +39,41 @@ profiles_dict = init_profiles()
 user_list = list(profiles_dict.keys())
 print(len(profiles_dict))
 
-############################################### HELPERS
+# HELPERS
+
 
 def make_response(r):
     return {'success': True, 'data': r}
 
+
 def make_boolean_response():
     return {'success': True}
 
-############################################### HELLO WORLD
+# HELLO WORLD
+
 
 @app.route('/', methods=['GET'])
 def hello_world():
     return 'Hello, World!'
 
-############################################### OAUTH
+# OAUTH
+
 
 def generate_oauth_url(redirect_uri):
     return '<a href="https://slack.com/oauth/v2/authorize?user_scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=1085915442018.1079182647094&redirect_uri={}"><img alt=""Sign in with Slack"" height="40" width="172" src="https://platform.slack-edge.com/img/sign_in_with_slack.png" srcset="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x" /></a>'.format(redirect_uri)
+
 
 @app.route('/signup', methods=['GET'])
 def signup():
     redirect_uri = os.environ["SERVER_URL"] + "/oauthv2"
     return generate_oauth_url(redirect_uri)
 
+
 @app.route('/signin', methods=['GET'])
 def signin():
     redirect_uri = os.environ["SERVER_URL"] + "/oauth"
     return generate_oauth_url(redirect_uri)
+
 
 def oauth_common(code, redirect_uri, base_url):
     CLIENT_ID = os.environ["CLIENT_ID"]
@@ -112,19 +118,16 @@ def oauthv2():
 
     return make_response(oauth_common(code, redirect_uri, base_url))
 
-############################################### OTHER ROUTES
+# OTHER ROUTES
 @app.route('/homepage', methods=['GET'])
 def homepage():
     user_id = request.args.get('user_id')
     req_user_profile = profiles_dict[user_id]
 
-    half_list_index = len(user_list) // 2
-    new_hires_pool = [profiles_dict[user_id] for user_id in user_list[:half_list_index]]
-    anniversaries_pool = [profiles_dict[user_id] for user_id in user_list[half_list_index:]]
-
     def similar_in_interest(p1, p2):
         return p1 != p2 and len(set(p1.interests).intersection(p2.interests)) > 0
-    similar_interests = [profile for profile in list(profiles_dict.values()) if similar_in_interest(profile, req_user_profile)]
+    similar_interests = [profile for profile in list(
+        profiles_dict.values()) if similar_in_interest(profile, req_user_profile)]
 
     return make_response({
         'anniversaries': [Anniversary(prof).serialize() for prof in list(profiles_dict.values())],
@@ -134,6 +137,7 @@ def homepage():
         'similarInterests': [prof.serialize(profiles_dict) for prof in similar_interests]
     })
 
+
 @app.route('/poll', methods=['POST'])
 def poll():
     sender = profiles_dict[request.json['user_id']]
@@ -142,8 +146,9 @@ def poll():
     poll_id = str(len(all_polls))
     all_polls[poll_id] = Poll(poll_id, sender, text, options)
 
-    broadcast_poll(poll_id, text, options, user_list)
+    slackbot.broadcast_poll(poll_id, text, options, user_list)
     return make_boolean_response()
+
 
 @app.route('/polls', methods=['GET'])
 def polls():
@@ -166,9 +171,10 @@ def get_users():
 def send_message():
     user_id = request.json['user_id']
     message = request.json['message']
-    send_dm_to_user(user_id, message)
+    slackbot.send_dm_to_user(user_id, message)
 
     return make_boolean_response()
+
 
 @app.route('/slack/poll', methods=['POST'])
 def receive_vote():
@@ -177,12 +183,13 @@ def receive_vote():
     poll_id = str(payload['message']['blocks'][0]['block_id'])
     option = payload['actions'][0]['text']['text']
     all_polls[poll_id].add_vote(option, user_id)
-    x = requests.post(payload['response_url'], json = {
+    x = requests.post(payload['response_url'], json={
         "text": poll_confirmation,
         "replace_original": "true"
     })
 
     return make_boolean_response()
+
 
 @app.route('/slack/thank', methods=['POST'])
 def thank():
@@ -192,15 +199,17 @@ def thank():
     recipient_name = message[1:space_bar_index]
     message = message[space_bar_index + 1:]
 
-    recipient_profile = next(prof for prof in list(profiles_dict.values()) if prof.slack_internal_name == recipient_name)
+    recipient_profile = next(prof for prof in list(
+        profiles_dict.values()) if prof.slack_internal_name == recipient_name)
 
     thanks.append(Thank(profiles_dict[sender_id], recipient_profile, message))
 
-    send_dm_to_user(
+    slackbot.send_dm_to_user(
         recipient_profile.id,
-        "<@{}> just thanked you for {}!".format(profiles_dict[sender_id].id, message)
+        "<@{}> just thanked you for {}!".format(
+            profiles_dict[sender_id].id, message)
     )
-    
+
     return {
         "blocks": [
             {
@@ -220,16 +229,18 @@ def team_join(payload):
     event = payload.get("event", {})
     user_id = event.get("user")
 
-    send_dm_to_user(user_id, "Welcome!")
-    send_dm_to_user(user_id, onboarding_questions[0])
+    slackbot.send_dm_to_user(user_id, "Welcome!")
+    slackbot.send_dm_to_user(user_id, onboarding_questions[0])
+
 
 @slack_events_adapter.on('reaction_added')
 def reaction_added(payload):
     event = payload.get("event", {})
     user_id = event.get("user")
 
-    send_dm_to_user(user_id, "Welcome!")
-    send_dm_to_user(user_id, onboarding_questions[0])
+    slackbot.send_dm_to_user(user_id, "Welcome!")
+    slackbot.send_dm_to_user(user_id, onboarding_questions[0])
+
 
 @slack_events_adapter.on('message')
 def message(payload):
@@ -240,28 +251,31 @@ def message(payload):
     channel_id = event.get("channel")
     user_id = event.get("user")
 
-    last_messages = messages_in_channel(channel_id, 2)
+    last_messages = slackbot.messages_in_channel(channel_id, 2)
     last_bot_question = next(msg for msg in last_messages if 'bot_id' in msg)
     answer = event['text']
 
     if last_bot_question['text'] == onboarding_questions[0]:
         profiles_dict[user_id].blurb = answer
-        send_dm_to_user(user_id, onboarding_questions[1])
+        slackbot.send_dm_to_user(user_id, onboarding_questions[1])
     elif last_bot_question['text'] == onboarding_questions[1]:
         interests = answer.split(',')
-        profiles_dict[user_id].interests = [interest.strip() for interest in interests]
+        profiles_dict[user_id].interests = [interest.strip()
+                                            for interest in interests]
     elif last_bot_question['text'] in daily_questions:
-        profiles_dict[user_id].daily_questions.append(DailyQuestion(last_bot_question['text'], answer))
-        send_dm_to_user(user_id, daily_q_confirmation)
+        profiles_dict[user_id].daily_questions.append(
+            DailyQuestion(last_bot_question['text'], answer))
+        slackbot.send_dm_to_user(user_id, daily_q_confirmation)
     elif last_bot_question['blocks'][0]['block_id'] in all_polls:
         poll = all_polls[last_bot_question['blocks'][0]['block_id']]
         poll.add_vote(answer, user_id)
-        send_dm_to_user(user_id, poll_confirmation)
+        slackbot.send_dm_to_user(user_id, poll_confirmation)
 
 # Error events
 @slack_events_adapter.on("error")
 def error_handler(err):
     print("ERROR: " + str(err))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
