c2c3187ebbb301eb2d2281b312f527c98920bdcb
Young Kim <yk465@cornell.edu>
2020-04-25 21:22:27 -0400

    hello

--- src/views/Profile/Components/ProfileHeader.tsx
+++ src/views/Profile/Components/ProfileHeader.tsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { AppState } from 'redux/reducer';
 import { useParams } from "react-router-dom"
 import { AppAction, Employee, DispatchProps } from "redux/types"
-import { Box, Grid, GridList, GridListTile, TextareaAutosize } from "@material-ui/core"
+import { Box, Grid, Link, GridList, GridListTile, TextareaAutosize } from "@material-ui/core"
 import { styled as muiStyled } from '@material-ui/core/styles'
 import styled from "styled-components"
 import { Button, Image, Text, Spacer } from "components"
@@ -35,10 +35,6 @@ export const ProfileHeaderComponent: React.FC<ProfileHeaderComponentProps> = ({ 
     ["Instagram", profile.instagram],
   ]
 
-  const messageUser = () => {
-
-  }
-
   return (
     // <Box display="flex" alignItems="center" justifyContent="space-around">
     <Grid container spacing={5}>
@@ -64,9 +60,11 @@ export const ProfileHeaderComponent: React.FC<ProfileHeaderComponentProps> = ({ 
           <Text variant="body1" color={colors.gray4}>{profile.interests.join(", ")}</Text>
           <Spacer mt={6} />
           <Box display="flex">
-            <Button backgroundColor={colors.purple} borderRadius={4} border={"none"} padding={12} onClick={messageUser}>
-              <Text color={colors.white100} variant="body1">{`Message ${profile.name.split(" ")[0]}`}</Text>
-            </Button>
+            <Link href={`slack://user?team=${profile.teamId}&id=${profile.id}`}>
+              <Button backgroundColor={colors.purple} borderRadius={4} border={"none"} padding={12}>
+                <Text color={colors.white100} variant="body1">{`Message ${profile.name.split(" ")[0]}`}</Text>
+              </Button>
+            </Link>
             <Spacer ml={1} />
             <Button backgroundColor={colors.white100} borderRadius={4} padding={12} border={`1px solid ${colors.purple}`}>
               <Text color={colors.purple} variant="body1">{"View Calendar ↗"}</Text>
