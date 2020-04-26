6b4f0c73fa7a3492a46e05eae42e7361d916e87d
Omar Rasheed <oyr2@cornell.edu>
2020-04-25 22:22:36 -0700

    Finish filtering search

--- src/views/Search/components/SearchResult.tsx
+++ src/views/Search/components/SearchResult.tsx
@@ -1,9 +1,9 @@
-import { Box } from "@material-ui/core"
-import { Spacer, Text } from "components"
-import React from "react"
+import { Box } from "@material-ui/core";
+import { Spacer, Text } from "components";
+import React from "react";
 import { useHistory } from "react-router-dom";
-import { Employee } from "redux/types"
-import { colors } from "theme/colors"
+import { Employee } from "redux/types";
+import { colors } from "theme/colors";
 
 export interface SearchResultProps {
   employee: Employee
@@ -29,7 +29,7 @@ export const SearchResultComponent: React.FC<SearchResultProps> = ({ employee })
         <Spacer ml={1} />
         <Text variant="body2" color={colors.gray4}>Dept</Text>
         <Spacer ml={0.5} />
-        <Text variant="body1" color={colors.purple}>Engineering</Text>
+        <Text variant="body1" color={colors.purple}>{employee.department}</Text>
       </Box>
     </Box>
   )
