eafff765d95cc6edd5f81441463985592d56766e
Omar Rasheed <oyr2@cornell.edu>
2020-04-25 19:17:32 -0700

    Change featured posts to array

--- backend/init_profiles.py
+++ backend/init_profiles.py
@@ -29,11 +29,13 @@ def init_profiles():
                 "url": "omarrasheed.com",
                 "description": "I write essays on my personal site every week. Feel free to check it out :)",
             },
-            {
+            [
+                {
                 "title": "Summer in Seattle!",
                 "body": "Getting dinner with Young Kim and Kevin Chan",
                 "imageURL": "https://files.slack.com/files-pri/T012HSXD00J-F012BKJ9JJJ/cd1f8e21-64bc-40ef-bb28-59d5559b328c_1_105_c.jpeg",
-            },
+                }
+            ],
             "Kevin's grandmas basement",
             get_team_icon(),
             get_team_id(),
