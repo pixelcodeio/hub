dbdd3cf03cc346230dfc233bb25822429e237821
Omar Rasheed <oyr2@cornell.edu>
2020-04-25 20:07:53 -0700

    work on filters

--- src/views/Search/SearchView.tsx
+++ src/views/Search/SearchView.tsx
@@ -1,7 +1,7 @@
-import { Box, Grid, Input } from "@material-ui/core";
+import { Box, Checkbox as MUICheckbox, Grid, Input } from "@material-ui/core";
 import { styled as muiStyled } from '@material-ui/core/styles';
 import { SearchBarIcon } from "assets";
-import { Spacer } from "components";
+import { Spacer, Text } from "components";
 import React, { useEffect } from "react";
 import { connect } from 'react-redux';
 import { setCurrentPage } from 'redux/actions';
@@ -15,6 +15,12 @@ export interface SearchViewComponentProps extends DispatchProps {
   user?: Employee
 }
 
+const teams_info = [
+  ["Growth", 1],
+  ["Customer Support", 1],
+  ["People & Culture", 1]
+]
+
 export const SearchViewComponent: React.FC<SearchViewComponentProps> = ({ dispatch, allEmployees, user }) => {
   useEffect(() => {
     dispatch(setCurrentPage("Search"))
@@ -23,7 +29,24 @@ export const SearchViewComponent: React.FC<SearchViewComponentProps> = ({ dispat
   return (
     <Box p={6}>
       <Grid container>
-        <Grid item xs={3} />
+        <Grid item xs={3} >
+          <FilterContainer p={2} display="flex" flexDirection="column">
+            <Box>
+              <Text variant="body1">Teams</Text>
+              <Spacer mt={1.5} />
+              {teams_info.map((team, index) => (
+                <Box key={index}>
+                  <Box display="flex" alignItems="center" justifyContent="space-between">
+                    <Text variant="body2" color={colors.gray4}>{`${team[0]} (${team[1]})`}</Text>
+                    <Checkbox disableRipple size="small" />
+                  </Box>
+                  <Spacer mt={1} />
+                </Box>
+              ))}
+            </Box>
+          </FilterContainer>
+        </Grid>
+        <Spacer ml={5} />
         <Grid item xs={6}>
           <Container py={2} px={4} display="flex" alignItems="center">
             <SearchBarIcon />
@@ -40,14 +63,32 @@ export const SearchViewComponent: React.FC<SearchViewComponentProps> = ({ dispat
         </Grid>
       </Grid>
 
-    </Box>
+    </Box >
   )
 }
+
+const FilterContainer = muiStyled(Box)({
+  borderRadius: "8px",
+  border: `1px solid ${colors.gray3}`,
+})
 
 const Container = muiStyled(Box)({
   borderRadius: "8px",
   border: `1px solid ${colors.gray3}`,
   boxShadow: "0px 8px 16px rgba(161, 163, 166, 0.08), 0px 4px 24px rgba(161, 163, 166, 0.08);",
+})
+
+const Checkbox = muiStyled(MUICheckbox)({
+  height: 12,
+  width: 12,
+  padding: 0,
+  color: colors.gray3,
+  "&:hover": {
+    backgroundColor: "transparent"
+  },
+  "&.Mui-checked": {
+    color: colors.purple
+  }
 })
 
 const mapStateToProps = (state: AppState) => ({
